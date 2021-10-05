d92d883d2912262a3faa74144a097c0747506787
Anton Andryushchenko <andryushchenko_anton@outlook.com>
2021-10-05 21:38:56 +0300

    [FIX] Code cleanup strange bugs

--- src/AInq.Helpers.Email/EmailHelper.cs
+++ src/AInq.Helpers.Email/EmailHelper.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System.Text.RegularExpressions;
+
 namespace AInq.Helpers.Email;
 
 /// <summary> Email <see cref="string" /> extension </summary>
--- src/AInq.Helpers.Polly/HttpRetryPolicies.cs
+++ src/AInq.Helpers.Polly/HttpRetryPolicies.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System.Net;
+
 namespace AInq.Helpers.Polly;
 
 /// <summary> Retry policies for HTTP requests </summary>
