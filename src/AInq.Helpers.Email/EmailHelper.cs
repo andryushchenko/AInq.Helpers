bccc2a49da2a1aa081781287166377d05da134e2
Anton Andryushchenko <andryushchenko_anton@outlook.com>
2025-04-03 09:58:36 +0300

    [ADD] .net9.0 [UPD] Dependencies

--- src/AInq.Helpers.Email/EmailHelper.cs
+++ src/AInq.Helpers.Email/EmailHelper.cs
@@ -17,18 +17,13 @@ using System.Text.RegularExpressions;
 namespace AInq.Helpers.Email;
 
 /// <summary> Email <see cref="string" /> extension </summary>
-#if NET7_0_OR_GREATER
-public static partial class EmailHelper
-#else
+#if NETSTANDARD
 public static class EmailHelper
+#else
+public static partial class EmailHelper
 #endif
 {
-#if NET7_0_OR_GREATER
-    [GeneratedRegex(
-        @"\s*(?<email>(?<user>\w[\w!#$%&'*/=?`{|}~^-]*(?:\.[\w!#$%&'*/=?`{|}~^-]+)*)(?<marker>\+[\w!#$%&'*/=?`{|}~^+\.-]*)?@(?<domain>(?:[a-z0-9-]+\.)+[a-z]{2,}))\s*",
-        RegexOptions.IgnoreCase | RegexOptions.NonBacktracking)]
-    private static partial Regex Pattern();
-#else
+#if NETSTANDARD
     private static readonly Lazy<Regex> PatternValue = new(() => new Regex(
         @"\s*(?<email>(?<user>\w[\w!#$%&'*/=?`{|}~^-]*(?:\.[\w!#$%&'*/=?`{|}~^-]+)*)(?<marker>\+[\w!#$%&'*/=?`{|}~^+\.-]*)?@(?<domain>(?:[a-z0-9-]+\.)+[a-z]{2,}))\s*",
         RegexOptions.IgnoreCase | RegexOptions.Compiled,
@@ -36,6 +31,11 @@ public static class EmailHelper
 
     private static Regex Pattern()
         => PatternValue.Value;
+#else
+    [GeneratedRegex(
+        @"\s*(?<email>(?<user>\w[\w!#$%&'*/=?`{|}~^-]*(?:\.[\w!#$%&'*/=?`{|}~^-]+)*)(?<marker>\+[\w!#$%&'*/=?`{|}~^+\.-]*)?@(?<domain>(?:[a-z0-9-]+\.)+[a-z]{2,}))\s*",
+        RegexOptions.IgnoreCase | RegexOptions.NonBacktracking)]
+    private static partial Regex Pattern();
 #endif
 
     /// <summary> Check is source string contains correct email </summary>
